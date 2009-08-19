d201c14f02fdae2cff3a1faabb6202198a61d984
sven@meiers.net <sven@meiers.net@2bdc7e2e-8969-11de-b317-bf1d2adb44e4>
2009-08-19 20:20:18 +0000

    moved to util package

--- trunk/wicket-dnd/src/main/java/wicketdnd/util/MarkupIdVisitor.java
+++ trunk/wicket-dnd/src/main/java/wicketdnd/util/MarkupIdVisitor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package wicketdnd;
+package wicketdnd.util;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.Component.IVisitor;
